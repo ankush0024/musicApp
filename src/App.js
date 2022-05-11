0dc27e4a8cc32f0eec77cf3214fe28504240f8af
ankush <ankush.kumar.singhal@gmail.com>
2022-05-12 00:47:36 +0530

    changes reverted

--- src/App.js
+++ src/App.js
@@ -13,77 +13,59 @@ const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
 function App() {
   const [token, setToken] = useState("");
-  const [searchKey, setSearchKey] = useState("");
-  const [artists, setArtists] = useState([]);
+  const [searchKey, setSearchKey] = useState("")
+  const [artists, setArtists] = useState([])
   useEffect(() => {
-    const hash = window.location.hash;
-    let token = window.localStorage.getItem("token");
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
 
     if (!token && hash) {
-      token = hash
-        .substring(1)
-        .split("&")
-        .find((elem) => elem.startsWith("access_token"))
-        .split("=")[1];
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
 
-      window.location.hash = "";
-      window.localStorage.setItem("token", token);
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
     }
 
-    setToken(token);
-  }, []);
+    setToken(token)
+
+  }, [])
   const logout = () => {
-    setToken("");
-    window.localStorage.removeItem("token");
-  };
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
   const searchArtists = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     const { data } = await axios.get("https://api.spotify.com/v1/search", {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token}`
       },
       params: {
         q: searchKey,
-        type: "artist",
-      },
-    });
+        type: "artist"
+      }
+    })
 
-    setArtists(data.artists.items);
-  };
+    setArtists(data.artists.items)
+  }
   const renderArtists = () => {
-    return artists.map((artist) => (
+    return artists.map(artist => (
       <div key={artist.id}>
-        {artist.images.length ? (
-          <img width={"100%"} src={artist.images[0].url} alt="" />
-        ) : (
-          <div>No Image</div>
-        )}
+        {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt="" /> : <div>No Image</div>}
         {artist.name}
       </div>
-    ));
-  };
+    ))
+  }
   return (
     <div className="App">
       <header className="App-header">
-        {!token ? (
-          <a
-            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-          >
-            Login to Spotify
-          </a>
-        ) : (
-          <div>
-            <form onSubmit={searchArtists}>
-              <input
-                type="text"
-                onChange={(e) => setSearchKey(e.target.value)}
-              />
-              <button type={"submit"}>Search</button>
-            </form>
-            {renderArtists()}
-            <button onClick={logout}>Logout</button>
-          </div>
-        )}
+        {!token ?
+          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+            to Spotify</a>
+          : <div><form onSubmit={searchArtists}>
+            <input type="text" onChange={e => setSearchKey(e.target.value)} />
+            <button type={"submit"}>Search</button>
+          </form>
+            {renderArtists()}<button onClick={logout}>Logout</button></div>}
       </header>
     </div>
   );
